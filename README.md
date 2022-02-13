a235fb1e9a04817bd42f6827a2bb80f8c71a23e6
ZhcnPanda <2623493447@qq.com>
2022-02-13 20:07:13 +0800

    Add extras for cezanne-user 11 RP1A.200720.011 V12.5.7.0.RJNCNXM release-keys
    Signed-off-by: ZhcnPanda <2623493447@qq.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## cezanne-user 11 RP1A.200720.011 V12.5.7.0.RJNCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6885
+- Codename: cezanne
+- Brand: Redmi
+- Flavor: cezanne-user
+- Release Version: 11
+- Id: RP1A.200720.011
+- Incremental: V12.5.7.0.RJNCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: false
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/cezanne/cezanne:11/RP1A.200720.011/V12.5.7.0.RJNCNXM:user/release-keys
+- OTA version: 
+- Branch: cezanne-user-11-RP1A.200720.011-V12.5.7.0.RJNCNXM-release-keys
+- Repo: redmi_cezanne_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/app/Mipay/Mipay.apk.* 2>/dev/null >> system/system/app/Mipay/Mipay.apk
+rm -f system/system/app/Mipay/Mipay.apk.* 2>/dev/null
+cat system/system/app/Music/Music.apk.* 2>/dev/null >> system/system/app/Music/Music.apk
+rm -f system/system/app/Music/Music.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null >> system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk
+rm -f system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null >> system/system/data-app/VipAccount/VipAccount.apk
+rm -f system/system/data-app/VipAccount/VipAccount.apk.* 2>/dev/null
+cat system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null >> system/system/data-app/MiFinance/MiFinance.apk
+rm -f system/system/data-app/MiFinance/MiFinance.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat vendor/camera/mimoji/model2.zip.* 2>/dev/null >> vendor/camera/mimoji/model2.zip
+rm -f vendor/camera/mimoji/model2.zip.* 2>/dev/null
